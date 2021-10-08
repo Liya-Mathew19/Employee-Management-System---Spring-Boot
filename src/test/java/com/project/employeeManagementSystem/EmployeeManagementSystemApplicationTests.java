28bf913f7adecafd8ef03b9719b9fe657c208c80
AbhiramiAppu <abhirami7696@gmail.com>
2021-10-08 18:47:09 +0530

    modified

---
+++ src/test/java/com/project/employeeManagementSystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.employeeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
